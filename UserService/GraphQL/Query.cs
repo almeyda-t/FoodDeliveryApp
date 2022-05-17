b6cd0e49d799f36d2289e7b71650ae5925d26b49
Almeyda Tara <Almeyda.Nabilla@atlantica.dev>
2022-05-17 16:31:35 +0700

    edit get profile

--- UserService/GraphQL/Query.cs
+++ UserService/GraphQL/Query.cs
@@ -24,38 +24,38 @@ namespace UserService.QraphQL
                 Username = p.Username
             });
 
-        //[Authorize]
-        //public IQueryable<Profile> GetProfilesbyToken([Service] FoodDeliveryAppContext context, ClaimsPrincipal claimsPrincipal)
-        //{
-        //    var userName = claimsPrincipal.Identity.Name;
-        //    var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
-        //    if (user != null)
-        //    {
-        //        var profiles = context.Profiles.Where(o => o.UserId == user.Id);
-        //        return profiles.AsQueryable();
-        //    }
-        //    return new List<Profile>().AsQueryable();
-        //}
-
         [Authorize]
-        public IQueryable<Profile> GetProfiles([Service] FoodDeliveryAppContext context, ClaimsPrincipal claimsPrincipal)
+        public IQueryable<Profile> GetProfilesbyToken([Service] FoodDeliveryAppContext context, ClaimsPrincipal claimsPrincipal)
         {
             var userName = claimsPrincipal.Identity.Name;
-
-            // check admin role ?
-            var adminRole = claimsPrincipal.Claims.Where(o => o.Type == ClaimTypes.Role && o.Value == "ADMIN").FirstOrDefault();
             var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
             if (user != null)
             {
-                if (adminRole != null)
-                {
-                    return context.Profiles;
-                }
                 var profiles = context.Profiles.Where(o => o.UserId == user.Id);
                 return profiles.AsQueryable();
             }
             return new List<Profile>().AsQueryable();
         }
+
+        //[Authorize]
+        //public IQueryable<Profile> GetProfiles([Service] FoodDeliveryAppContext context, ClaimsPrincipal claimsPrincipal)
+        //{
+        //    var userName = claimsPrincipal.Identity.Name;
+
+        //    // check admin role ?
+        //    var adminRole = claimsPrincipal.Claims.Where(o => o.Type == ClaimTypes.Role && o.Value == "ADMIN").FirstOrDefault();
+        //    var user = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+        //    if (user != null)
+        //    {
+        //        if (adminRole != null)
+        //        {
+        //            return context.Profiles;
+        //        }
+        //        var profiles = context.Profiles.Where(o => o.UserId == user.Id);
+        //        return profiles.AsQueryable();
+        //    }
+        //    return new List<Profile>().AsQueryable();
+        //}
 
     }
 }
