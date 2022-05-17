018b6bb9c163ac805d5fbaac02fbfbba280b2d2e
Almeyda Tara <Almeyda.Nabilla@atlantica.dev>
2022-05-17 14:41:12 +0700

    foodservice plus program orderservice

---
+++ FoodService/GraphQL/Query.cs
@@ -0,0 +1,22 @@
+﻿using FoodService.Models;
+using HotChocolate.AspNetCore.Authorization;
+
+namespace FoodService.GraphQL
+{
+    public class Query
+    {
+        [Authorize(Roles = new[] { "BUYER" })]
+        public IQueryable<Food> ViewFoods([Service] FoodDeliveryAppContext context) =>
+            context.Foods;
+
+        [Authorize(Roles = new[] { "BUYER" })]
+        public async Task<Food> ViewFoodByIdAsync(
+        int id,
+        [Service] FoodDeliveryAppContext context)
+        {
+            var food = context.Foods.Where(o => o.Id == id).FirstOrDefault();
+
+            return await Task.FromResult(food);
+        }
+    }
+}
